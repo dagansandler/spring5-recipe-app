662b9ecc53cfa9c14743639d03c952918ef38bb6
Dagan Sandler <dagansandler@gmail.com>
2018-02-27 15:38:36 +0200

    added missing unit of measures and findByDescription method

--- src/main/java/io/github/dagansandler/spring5recipeapp/repositories/CategoryRepository.java
+++ src/main/java/io/github/dagansandler/spring5recipeapp/repositories/CategoryRepository.java
@@ -3,5 +3,10 @@ package io.github.dagansandler.spring5recipeapp.repositories;
 import io.github.dagansandler.spring5recipeapp.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+    Optional<Category> findByDescription(String description);
+
 }
--- src/main/java/io/github/dagansandler/spring5recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/io/github/dagansandler/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -3,6 +3,10 @@ package io.github.dagansandler.spring5recipeapp.repositories;
 import io.github.dagansandler.spring5recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 
 }
