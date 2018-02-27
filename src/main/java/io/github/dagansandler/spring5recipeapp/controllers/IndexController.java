b4567fe3ee1e691d648806f8791250cb2ac64053
Dagan Sandler <dagansandler@gmail.com>
2018-02-27 15:56:44 +0200

    display recipe list in Thymeleaf template

--- src/main/java/io/github/dagansandler/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/io/github/dagansandler/spring5recipeapp/controllers/IndexController.java
@@ -1,13 +1,23 @@
 package io.github.dagansandler.spring5recipeapp.controllers;
 
+import io.github.dagansandler.spring5recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    private RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
     @RequestMapping({"/", "", "/index"})
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
+        model.addAttribute("recipes", recipeService.getAllRecipes());
+
         return "index";
     }
 
